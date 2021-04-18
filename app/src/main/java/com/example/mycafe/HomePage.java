46f0f784dfe40faf5b52b28a3cffc9ff859ac9df
kishoretheeraj <kishoretheeraj@gmail.com>
2021-04-18 15:46:35 +0530

    Sixth Commit -18.04.2021- kt (Bottom Navigation)

--- app/src/main/java/com/example/mycafe/HomePage.java
+++ app/src/main/java/com/example/mycafe/HomePage.java
@@ -1,8 +1,13 @@
 package com.example.mycafe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,7 +15,42 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.search:
+                    selectedFragment = new SearchFragment();
+                    break;
+                case R.id.cart:
+                    selectedFragment = new CartFragment();
+                    break;
+                case R.id.profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+
+
+            }
+
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
+
 
     public void onBackPressed() {
         finishAffinity();
